e1c1b9da27bcf165240c9f4821cdd14ecd42dbf1
Phenzic <juliusayoola5@gmail.com>
2025-11-06 23:38:09 +0100

    test error

--- src/services/nftService.ts
+++ src/services/nftService.ts
@@ -205,16 +205,7 @@ export async function getNFTMetadata(
     throw new Error(`Invalid object ID: ${objectId}`);
   }
 
-  try {
-    const object = await client.getObject({
-      id: objectId,
-      options: {
-        showContent: true,
-        showDisplay: true,
-        showType: true,
-        showOwner: true,
-      },
-    });
+
 
     return object;
   } catch (error: any) {
